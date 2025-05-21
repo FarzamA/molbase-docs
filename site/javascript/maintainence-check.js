7c2e52cecc5c24f23236496ed36ed0611fb687b4
FarzamA <farzam135@gmail.com>
2025-05-21 19:16:02 -0400

    update fetch to relative path

--- site/javascript/maintainence-check.js
+++ site/javascript/maintainence-check.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const statusEl = document.getElementById("app-status-indicator");
   if (!statusEl) return;
 
-  fetch("/status/status.txt")
+  fetch("status/status.txt")
     .then(res => res.text())
     .then(text => {
       if (text.includes("200 OK")) {
